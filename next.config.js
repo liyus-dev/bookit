1ddca196ba7dd309fc11765bd733da9a8d412e82
liyus.dev <liyus.dev@gmail.com>
2021-10-13 19:08:55 +0700

    Initial Commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,24 @@
 module.exports = {
-  reactStrictMode: true,
+  env: {
+    DB_LOCAL_URI: '',
+
+    STRIPE_API_KEY: '',
+    STRIPE_SECRET_KEY: '',
+
+    STRIPE_WEBHOOK_SECRET: '',
+
+    CLOUDINARY_CLOUD_NAME: '',
+    CLOUDINARY_API_KEY: '',
+    CLOUDINARY_API_SECRET :'',
+
+    SMTP_HOST: '',
+    SMTP_PORT: '',
+    SMTP_USER: '',
+    SMTP_PASSWORD: '',
+    STMP_FROM_NAME: '',
+    STMP_FROM_EMAIL: ''
+  },
+  images: {
+    domains: [''],
+  },
 }
